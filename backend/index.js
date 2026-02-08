94e97e920790a626890fdcb12b70be58e97c466f
Tsedenia <tsedenianega@gmail.com>
2026-02-08 18:16:44 +0300

    cors origin added

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://your-frontend-name.vercel.app", // Add your Vercel URL here after you deploy
+      "https://cozy-corners.onrender.com", // Add your Vercel URL here after you deploy
     ],
     credentials: true,
   }),
@@ -21,6 +21,7 @@ app.use(
 app.use(express.json({ limit: "40mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));   
 //simple health check
+app.get('/',(req,res)=>{    res.status(200).json({message:'Welcome to Cozy Corner API'})});
 app.get('/health',async (req,res)=>{
   
    try{
