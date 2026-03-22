cc057b88d369ad777ed6e22f1b41b49898110cf1
Tsedenia <tsedenianega@gmail.com>
2026-03-22 10:15:04 +0300

    update the product

--- .next/dev/static/chunks/app_page_tsx_f0ee5d67._.js
+++ .next/dev/static/chunks/app_page_tsx_f0ee5d67._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_33424c57._.js",
-  "static/chunks/app_globals_71f961d1.css"
+  "static/chunks/node_modules_b106fd0c._.js",
+  "static/chunks/_feb094ba._.js"
 ],
     source: "dynamic"
 });
